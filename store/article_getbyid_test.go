68105633c0776cc04321308558b9140c27d82c5b
Yashupadhyaya <uyash73@gmail.com>
2025-01-02 14:08:54 +0530

    Unit test generated by RoostGPT
    Using AI Model claude-3-5-sonnet-20240620

---
+++ store/article_getbyid_test.go
@@ -0,0 +1,143 @@
+package store
+
+import (
+	"errors"
+	"testing"
+	"github.com/jinzhu/gorm"
+	"github.com/raahii/golang-grpc-realworld-example/model"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+
+
+
+type Call struct {
+	Parent *Mock
+
+	// The name of the method that was or will be called.
+	Method string
+
+	// Holds the arguments of the method.
+	Arguments Arguments
+
+	// Holds the arguments that should be returned when
+	// this method is called.
+	ReturnArguments Arguments
+
+	// Holds the caller info for the On() call
+	callerInfo []string
+
+	// The number of times to return the return arguments when setting
+	// expectations. 0 means to always return the value.
+	Repeatability int
+
+	// Amount of times this call has been called
+	totalCalls int
+
+	// Call to this method can be optional
+	optional bool
+
+	// Holds a channel that will be used to block the Return until it either
+	// receives a message or is closed. nil means it returns immediately.
+	WaitFor <-chan time.Time
+
+	waitTime time.Duration
+
+	// Holds a handler used to manipulate arguments content that are passed by
+	// reference. It's useful when mocking methods such as unmarshalers or
+	// decoders.
+	RunFn func(Arguments)
+}
+
+type T struct {
+	common
+	isEnvSet bool
+	context  *testContext // For running tests and subtests.
+}
+
+
+type MockDB struct {
+	mock.Mock
+}
+
+
+
+
+
+
+
+
+type T struct {
+	common
+	isEnvSet bool
+	context  *testContext // For running tests and subtests.
+}
+func (m *MockDB) Find(out interface{}, where ...interface{}) *gorm.DB {
+	args := m.Called(out, where)
+	return args.Get(0).(*gorm.DB)
+}
+func (m *MockDB) Preload(column string, conditions ...interface{}) *gorm.DB {
+	args := m.Called(column, conditions)
+	return args.Get(0).(*gorm.DB)
+}
+func TestGetByID(t *testing.T) {
+	tests := []struct {
+		name            string
+		id              uint
+		mockSetup       func(*MockDB)
+		expectedError   error
+		expectedArticle *model.Article
+	}{
+		{
+			name: "Successfully retrieve an existing article by ID",
+			id:   1,
+			mockSetup: func(m *MockDB) {
+				m.On("Preload", "Tags").Return(m)
+				m.On("Preload", "Author").Return(m)
+				m.On("Find", mock.AnythingOfType("*model.Article"), uint(1)).Run(func(args mock.Arguments) {
+					arg := args.Get(0).(*model.Article)
+					*arg = model.Article{
+						Model:  gorm.Model{ID: 1},
+						Title:  "Test Article",
+						Tags:   []model.Tag{{Name: "test"}},
+						Author: model.User{Model: gorm.Model{ID: 1}, Username: "testuser"},
+					}
+				}).Return(m)
+			},
+			expectedError: nil,
+			expectedArticle: &model.Article{
+				Model:  gorm.Model{ID: 1},
+				Title:  "Test Article",
+				Tags:   []model.Tag{{Name: "test"}},
+				Author: model.User{Model: gorm.Model{ID: 1}, Username: "testuser"},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockDB := new(MockDB)
+			tt.mockSetup(mockDB)
+
+			dbWrapper := struct {
+				*MockDB
+			}{mockDB}
+
+			store := &ArticleStore{db: &dbWrapper}
+
+			article, err := store.GetByID(tt.id)
+
+			if tt.expectedError != nil {
+				assert.Error(t, err)
+				assert.Equal(t, tt.expectedError.Error(), err.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+
+			assert.Equal(t, tt.expectedArticle, article)
+
+			mockDB.AssertExpectations(t)
+		})
+	}
+}

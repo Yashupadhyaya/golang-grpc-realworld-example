227f7cac3d2b51d78309050b0dc787fc7c1c03ee
Yashupadhyaya <50600305+Yashupadhyaya@users.noreply.github.com>
2025-03-27 15:10:41 +0530

    Improved Unit test generated by RoostGPT
    Using AI Model roostgpt-4-32k, for user feedback: TEMP PROMPT 1 2 3 4 5 5 7 8 89  00  0 0

--- store/_user_test.go
+++ store/_user_test.go
@@ -1,9 +1,12 @@
+
 // ********RoostGPT********
 /*
 
 roost_feedback [3/27/2025, 2:36:24 PM]:Use these import statements:\r\n```\r\nimport (\r\n\terrors errors\r\n\truntime/debug\r\n\ttesting testing\r\n\r\n\tsqlmock github.com/DATA-DOG/go-sqlmock\r\n\tgorm github.com/jinzhu/gorm\r\n\tmodel github.com/raahii/golang-grpc-realworld-example/model\r\n\trequire github.com/stretchr/testify/require\r\n)\r\n```\r\n\r\n\r\nThe testing table in the TestUserStoreCreate function must be this:\r\n```\r\ntt := []struct {\r\n\t\tname     string\r\n\t\tuser     *model.User\r\n\t\tmock     func() (sqlmock.Sqlmock, error)\r\n\t\twantErr  bool\r\n\t\texpected *model.User\r\n\t}{\r\n\r\n\t\t{\r\n\t\t\tname: Scenario 1: Normal operation - Create a valid User,\r\n\t\t\tuser: &model.User{\r\n\t\t\t\tUsername: Alice,\r\n\t\t\t},\r\n\t\t\tmock: func() (sqlmock.Sqlmock, error) {\r\n\t\t\t\t_, mock, err := sqlmock.New()\r\n\t\t\t\tif err != nil {\r\n\t\t\t\t\treturn nil, err\r\n\t\t\t\t}\r\n\t\t\t\tmock.ExpectBegin()\r\n\t\t\t\tmock.ExpectExec(INSERT INTO `users` (.+) VALUES (.+)).\r\n\t\t\t\t\tWithArgs(Any, AnyWhere, Any)\r\n\t\t\t\tmock.ExpectCommit()\r\n\t\t\t\treturn mock, err\r\n\t\t\t},\r\n\t\t\twantErr:  false,\r\n\t\t\texpected: nil,\r\n\t\t},\r\n\t}\r\n```\r\n\r\nVERY IMPORTANT NOTE: DO NOT make any other change in the entire test code!!!
 
 roost_feedback [3/27/2025, 2:47:47 PM]:The mockDB struct must have this definition:\r\n```\r\ntype mockDB struct {\r\n}\r\n```\r\n\r\nThe testing table in the TestUserStoreCreate function must be this:\r\n```\r\ntt := []struct {\r\n\t\tname     string\r\n\t\tuser     *model.User\r\n\t\tmock     func() (sqlmock.Sqlmock, error)\r\n\t\twantErr  bool\r\n\t\texpected *model.User\r\n\t}{\r\n\t\t{\r\n\t\t\tname: Scenario 1: Normal operation - Create a valid User,\r\n\t\t\tuser: &model.User{\r\n\t\t\t\tUsername: Alice,\r\n\t\t\t},\r\n\t\t\tmock: func() (sqlmock.Sqlmock, error) {\r\n\t\t\t\t_, mock, err := sqlmock.New()\r\n\t\t\t\tif err != nil {\r\n\t\t\t\t\treturn nil, err\r\n\t\t\t\t}\r\n\t\t\t\tmock.ExpectBegin()\r\n\t\t\t\tmock.ExpectExec(INSERT INTO \users\ (.+) VALUES (.+)).\r\n\t\t\t\t\tWithArgs(Any, AnyWhere, Any)\r\n\t\t\t\tmock.ExpectCommit()\r\n\t\t\t\treturn mock, err\r\n\t\t\t},\r\n\t\t\twantErr:  false,\r\n\t\t\texpected: nil,\r\n\t\t},\r\n\t}\r\n```
+
+roost_feedback [3/27/2025, 3:08:27 PM]:TEMP PROMPT 1 2 3 4 5 5 7 8 89  00  0 0
 */
 
 // ********RoostGPT********
@@ -38,13 +41,13 @@ func TestUserStoreCreate(t *testing.T) {
 				Username: "Alice",
 			},
 			mock: func() (sqlmock.Sqlmock, error) {
-				db, mock, err := sqlmock.New()
+				_, mock, err := sqlmock.New()
 				if err != nil {
 					return nil, err
 				}
 				mock.ExpectBegin()
-				mock.ExpectExec("INSERT INTO \"users\" (.+) VALUES (.+)").
-					WithArgs(Any, AnyWhere, Any)
+				mock.ExpectExec("INSERT INTO "users" (.+) VALUES (.+)").
+					WithArgs("Any", "AnyWhere", "Any")
 				mock.ExpectCommit()
 				return mock, err
 			},
@@ -57,7 +60,8 @@ func TestUserStoreCreate(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			defer func() {
 				if r := recover(); r != nil {
-					t.Logf("Panic encountered so failing test. %v\n%s", r, debug.Stack())
+					t.Logf("Panic encountered so failing test. %v
+%s", r, debug.Stack())
 					t.Fail()
 				}
 			}()
